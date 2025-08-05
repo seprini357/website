152eee139e679bf38e989a39425b10172f315e23
seprini357 <isdwhsls7@gmail.com>
2025-08-05 22:25:57 +0900

    Asap홈페이지

--- src/App.js
+++ src/App.js
@@ -5,49 +5,17 @@ import Sellingpost from './pages/webpage/Sellingpost';
 import Paymentpage from './pages/webpage/Paymentpage';
 import PaymentComplete from './pages/webpage/PaymentComplete';
 import Mypage from './pages/webpage/Mypage';
-import ProtectedRoute from './components/webpage/ProtectedRoute';
-import { useState } from 'react';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   return (
     <div className="website-main-content">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/detail" element={<Detailpage />} />
-        <Route
-          path="/sellingpost"
-          element={
-            <ProtectedRoute isLoggedIn={isLoggedIn}>
-              <Sellingpost />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/payment"
-          element={
-            <ProtectedRoute isLoggedIn={isLoggedIn}>
-              <Paymentpage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/paymentcomplete"
-          element={
-            <ProtectedRoute isLoggedIn={isLoggedIn}>
-              <PaymentComplete />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/mypage"
-          element={
-            <ProtectedRoute isLoggedIn={isLoggedIn}>
-              <Mypage />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/sellingpost" element={<Sellingpost/>} />
+        <Route path="/payment" element={<Paymentpage/>} />
+        <Route path="/paymentcomplete" element={<PaymentComplete/>} />
+        <Route path="/mypage" element={<Mypage/>} />
       </Routes>
     </div>
   );
